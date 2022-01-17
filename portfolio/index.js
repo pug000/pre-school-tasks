8e55eb6e98277282d87bb8eb72c3f4a07bdd7a75
Roman Troshin <roman.troshin98@gmail.com>
2022-01-17 22:29:12 +0300

    feat: add adaptive hero section and burger menu

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,2 +1,15 @@
+const hamburgerBtn = document.querySelector('.hamburger');
+
+const toggleMenu = () => hamburgerBtn.classList.toggle('open');
+
+hamburgerBtn.addEventListener('click', toggleMenu);
+
+const listMenu = document.querySelector('.menu__list');
+
+hamburgerBtn.addEventListener('click', function () {
+  listMenu.classList.toggle('active');
+});
+
+
 const str = "Вёрстка валидная +10\nВёрстка семантическая +20\nВёрстка соответствует макету +48\nТребования к css +12\nИнтерактивность, реализуемая через css +20\nИтого: 110 баллов"
 console.log(str);
