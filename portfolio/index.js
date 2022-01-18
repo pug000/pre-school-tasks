12e99f8e2a0a8271fa3e35daae74a2fbf7356fac
Roman Troshin <roman.troshin98@gmail.com>
2022-01-18 10:28:52 +0300

    feat: complete burger menu

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,14 +1,39 @@
-const hamburgerBtn = document.querySelector('.hamburger');
+const menuBtn = document.querySelector('.hamburger__btn');
+const menu = document.querySelector('.menu__list');
+const menuOverlay = document.querySelector('.menu__overlay');
+const menuLink = document.querySelectorAll('.menu__link')
 
-const toggleMenu = () => hamburgerBtn.classList.toggle('open');
+function slider() {
+  if (menuBtn) {
+    menuBtn.addEventListener('click', function (e) {
+      document.body.classList.toggle('lock');
+      menuBtn.classList.toggle('active');
+      menu.classList.toggle('active')
+      menuOverlay.classList.toggle('active')
+    })
+  }
+}
+slider();
 
-hamburgerBtn.addEventListener('click', toggleMenu);
+menuLink.forEach(link => link.addEventListener('click', closeMenu));
 
-const listMenu = document.querySelector('.menu__list');
-
-hamburgerBtn.addEventListener('click', function () {
-  listMenu.classList.toggle('active');
-});
+function closeMenu() {
+  menuBtn.classList.remove('active');
+  document.body.classList.remove('lock');
+  menuBtn.classList.remove('active');
+  menu.classList.remove('active');
+  menuOverlay.classList.remove('active');
+  if (menuOverlay) {
+    menuOverlay.addEventListener('click', function (e) {
+      menuBtn.classList.remove('active');
+      document.body.classList.remove('lock');
+      menuBtn.classList.remove('active');
+      menu.classList.remove('active');
+      menuOverlay.classList.remove('active');
+    })
+  }
+}
+closeMenu()
 
 
 const str = "Вёрстка валидная +10\nВёрстка семантическая +20\nВёрстка соответствует макету +48\nТребования к css +12\nИнтерактивность, реализуемая через css +20\nИтого: 110 баллов"
