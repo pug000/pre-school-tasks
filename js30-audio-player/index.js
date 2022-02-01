c255b67e572167c3fdd7ca9558579de7624a76f6
Roman Troshin <roman.troshin98@gmail.com>
2022-02-01 17:20:15 +0300

    feat:add volume bar

--- js30-audio-player/index.js
+++ js30-audio-player/index.js
@@ -9,6 +9,10 @@ const nextBtn = document.querySelector('.next-song');
 const scaleImg = document.querySelector('.song-img');
 const currTime = document.querySelector('.current-time');
 const audioDuration = document.querySelector('.duration-time');
+const volumeBtn = document.querySelector('.volume-button')
+const volumeProgressContainer = document.querySelector('.volume-progress__container');
+const volumeProgress = document.querySelector('.volume-progress');
+const volumeBox = document.querySelector('.volume__box')
 let isPlay = false;
 
 
@@ -63,4 +67,28 @@ function getTimeAudio(number) {
   let min = parseInt(sec / 60);
   sec -= min * 60;
   return `${min}:${String(sec % 60).padStart(2, 0)}`;
+}
+
+//////////////////////////volume//////////////////////////
+volumeBtn.addEventListener('click', (event) => {
+  if (event.target.classList.contains('volume-button')) {
+    event.target.classList.toggle('muted');
+  }
+  event.target.classList.contains('muted') ? audio.muted = true : audio.muted = false;
+})
+
+volumeBox.onmouseover = () => {
+  volumeProgressContainer.style.height = '90px';
+}
+volumeBox.onmouseout = () => {
+  volumeProgressContainer.style.height = '0px';
+}
+
+volumeProgressContainer.addEventListener('click', changeVolume)
+
+function changeVolume(event) {
+  const lineHeight = window.getComputedStyle(volumeProgressContainer).height;
+  const volumeY = event.offsetY / parseInt(lineHeight);
+  audio.volume = volumeY;
+  volumeProgress.style.height = volumeY * 100 + '%';
 }
