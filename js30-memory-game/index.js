f8e3af126b33425817b66de02c3375c5d3985082
Roman Troshin <roman.troshin98@gmail.com>
2022-02-16 16:07:54 +0300

    feat: add random game

--- js30-memory-game/index.js
+++ js30-memory-game/index.js
@@ -1,17 +1,19 @@
 const cards = document.querySelectorAll('.game__card');
+let lockBoard = false;
 let hasFlippedCard = false;
 let firstCard, secondCard;
 
 cards.forEach(elem => elem.addEventListener('click', flipCard))
 
 function flipCard() {
+  if (this === firstCard) return;
+  if (lockBoard) return;
   this.classList.add('flip');
   if (!hasFlippedCard) {
     hasFlippedCard = true;
     firstCard = this;
   } else {
     secondCard = this;
-    hasFlippedCard = false;
     checkMatch();
   }
 }
@@ -20,18 +22,34 @@ function checkMatch() {
   if (firstCard.dataset.animal === secondCard.dataset.animal) {
     disableCards();
     return;
+  } else {
+    unflipCards();
   }
-  unflipCards();
 }
 
 function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
+  resetBoard();
 }
 
 function unflipCards() {
+  lockBoard = true;
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
-  }, 1500)
+    resetBoard();
+  }, 1000)
 }
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function getRandomGame() {
+  cards.forEach(elem => {
+    let randomCards = Math.floor(Math.random() * 20);
+    elem.style.order = randomCards;
+  });
+})();
